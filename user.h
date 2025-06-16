4a0d3b0bc40c9b519ee96ab2e4b5a55b02f4c14a
Alyaa Mahmoud <alyaamahmoud82@gmail.com>
2025-06-16 04:27:04 +0300

    user.h modified

--- user.h
+++ user.h
@@ -1,7 +1,6 @@
 #ifndef USER_H
 #define USER_H
 
-#include <QMetaType>
 #include <QString>
 #include <QDateTime>
 #include <QList>
@@ -69,6 +68,5 @@ private:
     int gamesTied;
     QList<GameRecord> gameHistory; // Now stores all games for database sync
 };
-Q_DECLARE_METATYPE(GameMove)
-Q_DECLARE_METATYPE(GameRecord)
+
 #endif // USER_H
